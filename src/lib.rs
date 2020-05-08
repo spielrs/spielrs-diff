7546ab106a98b77d97c970c973c9e3fd20b3c10b
spieljs <spieljs@gmail.com>
2020-05-08 18:15:10 +0200

    add dir_diff function

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,41 @@
-mod tree;
+pub mod tree;
+
+use tree::{Tree, TreeBuilder};
+
+pub async fn dir_diff(dir_path: String, dir_path_comp: String) -> bool {
+    let tree_one: Vec<Tree> = Tree::build_tree(dir_path).await;
+    let tree_two: Vec<Tree> = Tree::build_tree(dir_path_comp).await;
+    if !Tree::tree_diff(tree_one.clone(), tree_two.clone()) {
+        return true;
+    }
+
+    let content_one: Vec<String> = Tree::get_content_files(tree_one).await;
+    let content_two: Vec<String> = Tree::get_content_files(tree_two).await;
+    !Tree::compare_dir_content(content_one, content_two)
+}
+
+#[tokio::test]
+async fn should_return_true_if_both_dir_tree_are_different() {
+    let diff = dir_diff(
+        "./mocks/dir_one".to_string(),
+        "./mocks/dir_three".to_string(),
+    )
+    .await;
+    assert_eq!(diff, true);
+}
+
+#[tokio::test]
+async fn should_return_false_if_both_dir_tree_are_equal() {
+    let diff = dir_diff("./mocks/dir_one".to_string(), "./mocks/dir_two".to_string()).await;
+    assert_eq!(diff, false);
+}
+
+#[tokio::test]
+async fn should_return_true_if_both_dir_tree_have_different_content() {
+    let diff = dir_diff(
+        "./mocks/dir_one".to_string(),
+        "./mocks/dir_four".to_string(),
+    )
+    .await;
+    assert_eq!(diff, true);
+}
--- src/tree.rs
+++ src/tree.rs
@@ -5,14 +5,14 @@ use tokio::fs;
 use tokio::stream;
 
 #[async_trait]
-trait TreeBuilder {
+pub trait TreeBuilder {
     async fn build_tree(dir_path: String) -> Vec<Tree>;
     fn tree_diff(dir_tree: Vec<Tree>, dir_tree_comp: Vec<Tree>) -> bool;
     async fn get_content_files(dir_tree: Vec<Tree>) -> Vec<String>;
-    fn compare_dir_contents(dir_contents: Vec<String>, dir_contents_comp: Vec<String>) -> bool;
+    fn compare_dir_content(dir_content: Vec<String>, dir_content_comp: Vec<String>) -> bool;
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Tree {
     pub name: String,
     pub path: String,
@@ -20,17 +20,17 @@ pub struct Tree {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct TreeComp {
+struct TreeComp {
     pub name: String,
     pub subdir: Option<Vec<TreeComp>>,
 }
 
-pub struct ExtratedFile {
+struct ExtratedFile {
     pub name: String,
     pub path: String,
 }
 
-pub struct TreeFlatted(Vec<ExtratedFile>);
+struct TreeFlatted(Vec<ExtratedFile>);
 
 impl TreeFlatted {
     fn new() -> Self {
@@ -111,17 +111,17 @@ impl TreeBuilder for Tree {
         let file_list: TreeFlatted = TreeFlatted::from_iter(dir_tree);
         let files = stream::iter(file_list.0);
 
-        let file_contents: Vec<String> = files
+        let file_content: Vec<String> = files
             .then(|file| async { fs::read_to_string(file.path).await.unwrap() })
             .collect::<Vec<String>>()
             .await;
 
-        file_contents
+        file_content
     }
 
-    fn compare_dir_contents(dir_contents: Vec<String>, dir_contents_comp: Vec<String>) -> bool {
-        dir_contents.into_iter().all(move |content| {
-            dir_contents_comp
+    fn compare_dir_content(dir_content: Vec<String>, dir_content_comp: Vec<String>) -> bool {
+        dir_content.into_iter().all(move |content| {
+            dir_content_comp
                 .clone()
                 .into_iter()
                 .any(move |content_comp| content_comp == content)
@@ -150,12 +150,12 @@ async fn should_return_false_different_dir_tree() {
 }
 
 #[tokio::test]
-async fn should_return_all_file_contents() {
+async fn should_return_all_file_content() {
     let dir_one = Tree::build_tree("./mocks/dir_one".to_string()).await;
-    let contents = Tree::get_content_files(dir_one).await;
+    let content = Tree::get_content_files(dir_one).await;
 
     assert_eq!(
-        contents,
+        content,
         vec!(
             "Hello world",
             "print(\"This line will be printed.\")",
@@ -166,26 +166,23 @@ async fn should_return_all_file_contents() {
 }
 
 #[tokio::test]
-async fn should_return_true_if_both_dir_contents_are_equal() {
+async fn should_return_true_if_both_dir_content_are_equal() {
     let dir_one = Tree::build_tree("./mocks/dir_one".to_string()).await;
-    let contents_one = Tree::get_content_files(dir_one).await;
+    let content_one = Tree::get_content_files(dir_one).await;
 
     let dir_two = Tree::build_tree("./mocks/dir_two".to_string()).await;
-    let contents_two = Tree::get_content_files(dir_two).await;
+    let content_two = Tree::get_content_files(dir_two).await;
 
-    assert_eq!(Tree::compare_dir_contents(contents_one, contents_two), true);
+    assert_eq!(Tree::compare_dir_content(content_one, content_two), true);
 }
 
 #[tokio::test]
-async fn should_return_false_if_both_dir_contents_are_differents() {
+async fn should_return_false_if_both_dir_content_are_differents() {
     let dir_one = Tree::build_tree("./mocks/dir_one".to_string()).await;
-    let contents_one = Tree::get_content_files(dir_one).await;
+    let content_one = Tree::get_content_files(dir_one).await;
 
     let dir_four = Tree::build_tree("./mocks/dir_four".to_string()).await;
-    let contents_four = Tree::get_content_files(dir_four).await;
+    let content_four = Tree::get_content_files(dir_four).await;
 
-    assert_eq!(
-        Tree::compare_dir_contents(contents_one, contents_four),
-        false
-    );
+    assert_eq!(Tree::compare_dir_content(content_one, content_four), false);
 }
